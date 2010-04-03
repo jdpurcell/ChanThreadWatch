68a93e48190f28d330f8cbff8b680e7b7d174afb
J.D. Purcell <jdpurcell@gmail.com>
2010-04-03 02:49:53 +0000

    1.0.0

---
+++ frmChanThreadWatch.Designer.cs
@@ -0,0 +1,276 @@
+namespace ChanThreadWatch {
+	partial class frmChanThreadWatch {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.lvThreads = new System.Windows.Forms.ListView();
+			this.chURL = new System.Windows.Forms.ColumnHeader();
+			this.chStatus = new System.Windows.Forms.ColumnHeader();
+			this.grpAddThread = new System.Windows.Forms.GroupBox();
+			this.lblCheckEvery = new System.Windows.Forms.Label();
+			this.cboCheckEvery = new System.Windows.Forms.ComboBox();
+			this.txtImageAuth = new System.Windows.Forms.TextBox();
+			this.txtPageAuth = new System.Windows.Forms.TextBox();
+			this.chkImageAuth = new System.Windows.Forms.CheckBox();
+			this.chkPageAuth = new System.Windows.Forms.CheckBox();
+			this.chkOneTime = new System.Windows.Forms.CheckBox();
+			this.lblURL = new System.Windows.Forms.Label();
+			this.txtPageURL = new System.Windows.Forms.TextBox();
+			this.btnAdd = new System.Windows.Forms.Button();
+			this.btnStopSelected = new System.Windows.Forms.Button();
+			this.btnRemoveCompleted = new System.Windows.Forms.Button();
+			this.btnAbout = new System.Windows.Forms.Button();
+			this.btnOpenSelectedFolder = new System.Windows.Forms.Button();
+			this.grpAddThread.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// lvThreads
+			// 
+			this.lvThreads.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chURL,
+            this.chStatus});
+			this.lvThreads.FullRowSelect = true;
+			this.lvThreads.HideSelection = false;
+			this.lvThreads.Location = new System.Drawing.Point(8, 8);
+			this.lvThreads.Name = "lvThreads";
+			this.lvThreads.Size = new System.Drawing.Size(620, 164);
+			this.lvThreads.TabIndex = 0;
+			this.lvThreads.UseCompatibleStateImageBehavior = false;
+			this.lvThreads.View = System.Windows.Forms.View.Details;
+			// 
+			// chURL
+			// 
+			this.chURL.Text = "URL";
+			this.chURL.Width = 300;
+			// 
+			// chStatus
+			// 
+			this.chStatus.Text = "Status";
+			this.chStatus.Width = 300;
+			// 
+			// grpAddThread
+			// 
+			this.grpAddThread.Controls.Add(this.lblCheckEvery);
+			this.grpAddThread.Controls.Add(this.cboCheckEvery);
+			this.grpAddThread.Controls.Add(this.txtImageAuth);
+			this.grpAddThread.Controls.Add(this.txtPageAuth);
+			this.grpAddThread.Controls.Add(this.chkImageAuth);
+			this.grpAddThread.Controls.Add(this.chkPageAuth);
+			this.grpAddThread.Controls.Add(this.chkOneTime);
+			this.grpAddThread.Controls.Add(this.lblURL);
+			this.grpAddThread.Controls.Add(this.txtPageURL);
+			this.grpAddThread.Controls.Add(this.btnAdd);
+			this.grpAddThread.Location = new System.Drawing.Point(8, 176);
+			this.grpAddThread.Name = "grpAddThread";
+			this.grpAddThread.Size = new System.Drawing.Size(360, 157);
+			this.grpAddThread.TabIndex = 1;
+			this.grpAddThread.TabStop = false;
+			this.grpAddThread.Text = "Add Thread";
+			// 
+			// lblCheckEvery
+			// 
+			this.lblCheckEvery.AutoSize = true;
+			this.lblCheckEvery.Location = new System.Drawing.Point(12, 128);
+			this.lblCheckEvery.Name = "lblCheckEvery";
+			this.lblCheckEvery.Size = new System.Drawing.Size(119, 13);
+			this.lblCheckEvery.TabIndex = 7;
+			this.lblCheckEvery.Text = "Check every (minutes):";
+			// 
+			// cboCheckEvery
+			// 
+			this.cboCheckEvery.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cboCheckEvery.FormattingEnabled = true;
+			this.cboCheckEvery.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "5",
+            "10"});
+			this.cboCheckEvery.Location = new System.Drawing.Point(140, 124);
+			this.cboCheckEvery.Name = "cboCheckEvery";
+			this.cboCheckEvery.Size = new System.Drawing.Size(52, 21);
+			this.cboCheckEvery.TabIndex = 8;
+			// 
+			// txtImageAuth
+			// 
+			this.txtImageAuth.Enabled = false;
+			this.txtImageAuth.Location = new System.Drawing.Point(164, 72);
+			this.txtImageAuth.Name = "txtImageAuth";
+			this.txtImageAuth.Size = new System.Drawing.Size(184, 21);
+			this.txtImageAuth.TabIndex = 5;
+			// 
+			// txtPageAuth
+			// 
+			this.txtPageAuth.Enabled = false;
+			this.txtPageAuth.Location = new System.Drawing.Point(164, 44);
+			this.txtPageAuth.Name = "txtPageAuth";
+			this.txtPageAuth.Size = new System.Drawing.Size(184, 21);
+			this.txtPageAuth.TabIndex = 3;
+			// 
+			// chkImageAuth
+			// 
+			this.chkImageAuth.AutoSize = true;
+			this.chkImageAuth.Location = new System.Drawing.Point(12, 74);
+			this.chkImageAuth.Name = "chkImageAuth";
+			this.chkImageAuth.Size = new System.Drawing.Size(143, 17);
+			this.chkImageAuth.TabIndex = 4;
+			this.chkImageAuth.Text = "Image auth (user:pass):";
+			this.chkImageAuth.UseVisualStyleBackColor = true;
+			this.chkImageAuth.CheckedChanged += new System.EventHandler(this.chkImageAuth_CheckedChanged);
+			// 
+			// chkPageAuth
+			// 
+			this.chkPageAuth.AutoSize = true;
+			this.chkPageAuth.Location = new System.Drawing.Point(12, 46);
+			this.chkPageAuth.Name = "chkPageAuth";
+			this.chkPageAuth.Size = new System.Drawing.Size(137, 17);
+			this.chkPageAuth.TabIndex = 2;
+			this.chkPageAuth.Text = "Page auth (user:pass):";
+			this.chkPageAuth.UseVisualStyleBackColor = true;
+			this.chkPageAuth.CheckedChanged += new System.EventHandler(this.chkPageAuth_CheckedChanged);
+			// 
+			// chkOneTime
+			// 
+			this.chkOneTime.AutoSize = true;
+			this.chkOneTime.Location = new System.Drawing.Point(12, 100);
+			this.chkOneTime.Name = "chkOneTime";
+			this.chkOneTime.Size = new System.Drawing.Size(185, 17);
+			this.chkOneTime.TabIndex = 6;
+			this.chkOneTime.Text = "Don\'t watch (one-time download)";
+			this.chkOneTime.UseVisualStyleBackColor = true;
+			this.chkOneTime.CheckedChanged += new System.EventHandler(this.chkOneTime_CheckedChanged);
+			// 
+			// lblURL
+			// 
+			this.lblURL.AutoSize = true;
+			this.lblURL.Location = new System.Drawing.Point(10, 22);
+			this.lblURL.Name = "lblURL";
+			this.lblURL.Size = new System.Drawing.Size(30, 13);
+			this.lblURL.TabIndex = 0;
+			this.lblURL.Text = "URL:";
+			// 
+			// txtPageURL
+			// 
+			this.txtPageURL.Location = new System.Drawing.Point(48, 18);
+			this.txtPageURL.Name = "txtPageURL";
+			this.txtPageURL.Size = new System.Drawing.Size(300, 21);
+			this.txtPageURL.TabIndex = 1;
+			// 
+			// btnAdd
+			// 
+			this.btnAdd.Location = new System.Drawing.Point(264, 122);
+			this.btnAdd.Name = "btnAdd";
+			this.btnAdd.Size = new System.Drawing.Size(84, 23);
+			this.btnAdd.TabIndex = 9;
+			this.btnAdd.Text = "Add Thread";
+			this.btnAdd.UseVisualStyleBackColor = true;
+			this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+			// 
+			// btnStopSelected
+			// 
+			this.btnStopSelected.Location = new System.Drawing.Point(536, 180);
+			this.btnStopSelected.Name = "btnStopSelected";
+			this.btnStopSelected.Size = new System.Drawing.Size(92, 23);
+			this.btnStopSelected.TabIndex = 2;
+			this.btnStopSelected.Text = "Stop Selected";
+			this.btnStopSelected.UseVisualStyleBackColor = true;
+			this.btnStopSelected.Click += new System.EventHandler(this.btnStopSelected_Click);
+			// 
+			// btnRemoveCompleted
+			// 
+			this.btnRemoveCompleted.Location = new System.Drawing.Point(508, 212);
+			this.btnRemoveCompleted.Name = "btnRemoveCompleted";
+			this.btnRemoveCompleted.Size = new System.Drawing.Size(120, 23);
+			this.btnRemoveCompleted.TabIndex = 3;
+			this.btnRemoveCompleted.Text = "Remove Completed";
+			this.btnRemoveCompleted.UseVisualStyleBackColor = true;
+			this.btnRemoveCompleted.Click += new System.EventHandler(this.btnRemoveCompleted_Click);
+			// 
+			// btnAbout
+			// 
+			this.btnAbout.Location = new System.Drawing.Point(568, 310);
+			this.btnAbout.Name = "btnAbout";
+			this.btnAbout.Size = new System.Drawing.Size(60, 23);
+			this.btnAbout.TabIndex = 5;
+			this.btnAbout.Text = "About";
+			this.btnAbout.UseVisualStyleBackColor = true;
+			this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+			// 
+			// btnOpenSelectedFolder
+			// 
+			this.btnOpenSelectedFolder.Location = new System.Drawing.Point(496, 244);
+			this.btnOpenSelectedFolder.Name = "btnOpenSelectedFolder";
+			this.btnOpenSelectedFolder.Size = new System.Drawing.Size(132, 23);
+			this.btnOpenSelectedFolder.TabIndex = 4;
+			this.btnOpenSelectedFolder.Text = "Open Selected Folder";
+			this.btnOpenSelectedFolder.UseVisualStyleBackColor = true;
+			this.btnOpenSelectedFolder.Click += new System.EventHandler(this.btnOpenSelectedFolder_Click);
+			// 
+			// frmChanThreadWatch
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(636, 341);
+			this.Controls.Add(this.btnOpenSelectedFolder);
+			this.Controls.Add(this.btnAbout);
+			this.Controls.Add(this.btnRemoveCompleted);
+			this.Controls.Add(this.btnStopSelected);
+			this.Controls.Add(this.grpAddThread);
+			this.Controls.Add(this.lvThreads);
+			this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "frmChanThreadWatch";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Chan Thread Watch";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmChanThreadWatch_FormClosed);
+			this.Load += new System.EventHandler(this.frmChanThreadWatch_Load);
+			this.grpAddThread.ResumeLayout(false);
+			this.grpAddThread.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListView lvThreads;
+		private System.Windows.Forms.ColumnHeader chURL;
+		private System.Windows.Forms.ColumnHeader chStatus;
+		private System.Windows.Forms.GroupBox grpAddThread;
+		private System.Windows.Forms.CheckBox chkOneTime;
+		private System.Windows.Forms.Label lblURL;
+		private System.Windows.Forms.TextBox txtPageURL;
+		private System.Windows.Forms.Button btnAdd;
+		private System.Windows.Forms.Button btnStopSelected;
+		private System.Windows.Forms.Button btnRemoveCompleted;
+		private System.Windows.Forms.Button btnAbout;
+		private System.Windows.Forms.TextBox txtPageAuth;
+		private System.Windows.Forms.CheckBox chkImageAuth;
+		private System.Windows.Forms.CheckBox chkPageAuth;
+		private System.Windows.Forms.ComboBox cboCheckEvery;
+		private System.Windows.Forms.TextBox txtImageAuth;
+		private System.Windows.Forms.Label lblCheckEvery;
+		private System.Windows.Forms.Button btnOpenSelectedFolder;
+	}
+}
+
---
+++ frmChanThreadWatch.cs
@@ -0,0 +1,474 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace ChanThreadWatch {
+	public partial class frmChanThreadWatch : Form {
+		private List<WatchInfo> _watchInfoList = new List<WatchInfo>();
+
+		// Can't lock or Join in the UI thread because if it gets stuck waiting and a worker thread
+		// tries to Invoke, it will never return because Invoke needs to run on the (frozen) UI
+		// thread.  And I don't like the idea of BeginInvoke in this particular situation.
+
+		public frmChanThreadWatch() {
+			InitializeComponent();
+		}
+
+		private void frmChanThreadWatch_Load(object sender, EventArgs e) {
+			cboCheckEvery.SelectedItem = "3";
+		}
+
+		private void frmChanThreadWatch_FormClosed(object sender, FormClosedEventArgs e) {
+			while (!Monitor.TryEnter(_watchInfoList, 10)) Application.DoEvents();
+			foreach (WatchInfo w in _watchInfoList) {
+				w.Stop = true;
+			}
+			Monitor.Exit(_watchInfoList);
+			foreach (WatchInfo w in _watchInfoList) {
+				while ((w.WatchThread != null) && w.WatchThread.IsAlive) {
+					Thread.Sleep(10);
+					Application.DoEvents();
+				}
+			}
+		}
+
+		private void btnAdd_Click(object sender, EventArgs e) {
+			WatchInfo watchInfo = new WatchInfo();
+			string pageURL = txtPageURL.Text.Trim().ToLower();
+			int listIndex = -1;
+
+			if (pageURL.Length == 0) return;
+			if (!pageURL.StartsWith("http://")) pageURL = "http://" + pageURL;
+
+			while (!Monitor.TryEnter(_watchInfoList, 10)) Application.DoEvents();
+			try {
+				foreach (WatchInfo w in _watchInfoList) {
+					if (w.PageURL == pageURL) {
+						listIndex = ((w.WatchThread != null) && w.WatchThread.IsAlive) ? -2 : w.ListIndex;
+						break;
+					}
+				}
+				if (listIndex != -2) {
+					if (listIndex == -1) {
+						lvThreads.Items.Add(new ListViewItem(pageURL)).SubItems.Add(String.Empty);
+						_watchInfoList.Add(watchInfo);
+						listIndex = _watchInfoList.Count - 1;
+					}
+					else {
+						_watchInfoList[listIndex].ListIndex = -1;
+						_watchInfoList[listIndex] = watchInfo;
+					}
+					watchInfo.PageURL = pageURL;
+					watchInfo.PageAuth = (chkPageAuth.Checked && (txtPageAuth.Text.IndexOf(':') != -1)) ?
+						txtPageAuth.Text : String.Empty;
+					watchInfo.ImageAuth = (chkImageAuth.Checked && (txtImageAuth.Text.IndexOf(':') != -1)) ?
+						txtImageAuth.Text : String.Empty;
+					watchInfo.WaitSeconds = Int32.Parse((string)cboCheckEvery.SelectedItem) * 60;
+					watchInfo.OneTime = chkOneTime.Checked;
+					watchInfo.ListIndex = listIndex;
+					watchInfo.WatchThread = new Thread(WatchThread);
+				}
+			}
+			finally {
+				Monitor.Exit(_watchInfoList);
+			}
+
+			if (listIndex == -2) {
+				MessageBox.Show("The same thread is already being watched or downloaded.",
+					"Duplicate Thread", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			watchInfo.WatchThread.Start(watchInfo);
+			txtPageURL.Clear();
+			txtPageURL.Focus();
+		}
+		
+		private void btnStopSelected_Click(object sender, EventArgs e) {
+			while (!Monitor.TryEnter(_watchInfoList, 10)) Application.DoEvents();
+			try {
+				foreach (ListViewItem item in lvThreads.SelectedItems) {
+					_watchInfoList[item.Index].Stop = true;
+				}
+			}
+			finally {
+				Monitor.Exit(_watchInfoList);
+			}
+		}
+
+		private void btnRemoveCompleted_Click(object sender, EventArgs e) {
+			while (!Monitor.TryEnter(_watchInfoList, 10)) Application.DoEvents();
+			try {
+				int i = 0;
+				while (i < _watchInfoList.Count) {
+					WatchInfo watchInfo = _watchInfoList[i];
+					if (watchInfo.Stop || !watchInfo.WatchThread.IsAlive) {
+						watchInfo.ListIndex = -1;
+						_watchInfoList.RemoveAt(i);
+						lvThreads.Items.RemoveAt(i);
+					}
+					else {
+						watchInfo.ListIndex = i;
+						i++;
+					}
+				}
+			}
+			finally {
+				Monitor.Exit(_watchInfoList);
+			}
+		}
+
+		private void btnOpenSelectedFolder_Click(object sender, EventArgs e) {
+			while (!Monitor.TryEnter(_watchInfoList, 10)) Application.DoEvents();
+			try {
+				foreach (ListViewItem item in lvThreads.SelectedItems) {
+					string saveDir = _watchInfoList[item.Index].SaveDir;
+					if (!String.IsNullOrEmpty(saveDir)) {
+						try {
+							System.Diagnostics.Process.Start(saveDir);
+						}
+						catch {}
+					}
+				}
+			}
+			finally {
+				Monitor.Exit(_watchInfoList);
+			}
+		}
+
+		private void btnAbout_Click(object sender, EventArgs e) {
+			MessageBox.Show(String.Format("Chan Thread Watch{0}Version 1.0.0 (2007-Dec-05){0}jart1126@yahoo.com",
+				Environment.NewLine), "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+
+		private void chkOneTime_CheckedChanged(object sender, EventArgs e) {
+			cboCheckEvery.Enabled = !chkOneTime.Checked;
+		}
+
+		private void chkPageAuth_CheckedChanged(object sender, EventArgs e) {
+			txtPageAuth.Enabled = chkPageAuth.Checked;
+		}
+
+		private void chkImageAuth_CheckedChanged(object sender, EventArgs e) {
+			txtImageAuth.Enabled = chkImageAuth.Checked;
+		}
+
+		private void SetStatus(WatchInfo watchInfo, string status) {
+			if (watchInfo.ListIndex == -1) return;
+			Invoke((MethodInvoker)delegate() {
+				lvThreads.Items[watchInfo.ListIndex].SubItems[1].Text = status;
+			});
+		}
+
+		private byte[] StreamToBytes(Stream stream, FileStream fs) {
+			List<ByteBuff> buffList = new List<ByteBuff>();
+			while (true) {
+				byte[] data = new byte[8192];
+				int dataLen = stream.Read(data, 0, data.Length);
+				if (dataLen == 0) break;
+				buffList.Add(new ByteBuff(data, dataLen));
+				if (fs != null) {
+					fs.Write(data, 0, dataLen);
+				}
+			}
+			return ByteBuffListToBytes(buffList);
+		}
+
+		private byte[] ByteBuffListToBytes(List<ByteBuff> list) {
+			int totalLen = 0;
+			int offset = 0;
+			byte[] ret;
+
+			for (int i = 0; i < list.Count; i++) {
+				totalLen += list[i].Length;
+			}
+
+			ret = new byte[totalLen];
+
+			for (int i = 0; i < list.Count; i++) {
+				byte[] data = list[i].Data;
+				int len = list[i].Length;
+				Buffer.BlockCopy(data, 0, ret, offset, len);
+				offset += len;
+			}
+			
+			return ret;
+		}
+
+		private string BytesToString(byte[] bytes) {
+			byte[] copy = new byte[bytes.Length];
+			bool prevWasSpace = false;
+			int iDst = 0;
+			for (int iSrc = 0; iSrc < bytes.Length; iSrc++) {
+				if ((bytes[iSrc] == 32) || (bytes[iSrc] == 9) || (bytes[iSrc] == 13) || (bytes[iSrc] == 10)) {
+					if (!prevWasSpace) {
+						copy[iDst++] = 32;
+					}
+					prevWasSpace = true;
+				}
+				else {
+					copy[iDst++] = bytes[iSrc];
+					prevWasSpace = false;
+				}
+			}
+			return Encoding.ASCII.GetString(copy, 0, iDst);
+		}
+
+		private Stream GetToStream(string url, string auth, ref DateTime? cacheTime) {
+			HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+			if (cacheTime != null) {
+				req.IfModifiedSince = (DateTime)cacheTime;
+			}
+			if (!String.IsNullOrEmpty(auth)) {
+				req.Headers.Add("Authorization", "Basic " +
+					Convert.ToBase64String(Encoding.ASCII.GetBytes(auth)));
+			}
+			HttpWebResponse resp;
+			try {
+				resp = (HttpWebResponse)req.GetResponse();
+				cacheTime = resp.LastModified;
+			}
+			catch (WebException ex) {
+				if (ex.Status == WebExceptionStatus.ProtocolError) {
+					HttpStatusCode code = ((HttpWebResponse)ex.Response).StatusCode;
+					if (code == HttpStatusCode.NotFound) {
+						throw new HTTP404Exception();
+					}
+					else if (code == HttpStatusCode.NotModified) {
+						throw new HTTP304Exception();
+					}
+				}
+				throw;
+			}
+			return resp.GetResponseStream();
+		}
+
+		private Stream GetToStream(string url, string auth) {
+			DateTime? cacheTime = null;
+			return GetToStream(url, auth, ref cacheTime);
+		}
+
+		private string GetToString(string url, string auth, string path, ref DateTime? cacheTime) {
+			byte[] respBytes;
+			using (Stream respStream = GetToStream(url, auth, ref cacheTime)) {
+				if (path != null) {
+					using (FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Read)) {
+						respBytes = StreamToBytes(respStream, fs);
+					}
+				}
+				else {
+					respBytes = StreamToBytes(respStream, null);
+				}
+			}
+			return BytesToString(respBytes);
+		}
+
+		private void GetToFile(string url, string auth, string path) {
+			using (Stream respStream = GetToStream(url, auth)) {
+				using (FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Read)) {
+					while (true) {
+						byte[] data = new byte[8192];
+						int dataLen = respStream.Read(data, 0, data.Length);
+						if (dataLen == 0) break;
+						fs.Write(data, 0, dataLen);
+					}
+				}
+			}
+		}
+
+		private string AbsoluteURL(string pageURL, string linkURL) {
+			try {
+				return new Uri(new Uri(pageURL), linkURL).AbsoluteUri;
+			}
+			catch {
+				return linkURL;
+			}
+		}
+
+		private List<string> GetLinks(string page, string pageURL) {
+			List<string> links = new List<string>();
+			Regex r = new Regex("href\\s*=\\s*(?:(?:\\\"(?<url>[^\\\"]*)\\\")|(?<url>[^\\s]* ))");
+			MatchCollection mc = r.Matches(page);
+			foreach (Match m in mc) {
+				links.Add(AbsoluteURL(pageURL, m.Groups[1].Value));
+			}
+			return links;
+		}
+
+		private string URLFilename(string url) {
+			int pos = url.LastIndexOf("/");
+			return (pos == -1) ? String.Empty : url.Substring(pos + 1);
+		}
+
+		private void ParseURL(string url, out string site, out string board, out string thread) {
+			string[] urlSplit = url.Substring(7).Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+			site = String.Empty;
+			board = String.Empty;
+			thread = String.Empty;
+			if (urlSplit.Length >= 1) {
+				string[] hostSplit = urlSplit[0].Split('.');
+				if (hostSplit.Length >= 2) {
+					site = hostSplit[hostSplit.Length - 2];
+				}
+			}
+			if (urlSplit.Length >= 2) {
+				board = urlSplit[1];
+			}
+			if (urlSplit.Length >= 3) {
+				thread = urlSplit[urlSplit.Length - 1];
+				int pos = thread.LastIndexOf('.');
+				if (pos != -1) {
+					thread = thread.Substring(0, pos);
+				}
+			}
+		}
+
+		private void WatchThread(object p) {
+			WatchInfo watchInfo = (WatchInfo)p;
+			string pageURL = watchInfo.PageURL;
+			string pageAuth = watchInfo.PageAuth;
+			string imgAuth = watchInfo.ImageAuth;
+			double waitSeconds = (double)watchInfo.WaitSeconds;
+			string page = null;
+			string saveDir, saveFilename, savePath, site, board, thread;
+			int numTries;
+			const int maxTries = 3;
+			DateTime pageGetTime = DateTime.Now;
+			DateTime? pageCacheTime = null;
+			double waitRemain;
+
+			ParseURL(pageURL, out site, out board, out thread);
+			saveDir = Path.Combine(Application.StartupPath, site + "_" + board + "_" + thread);
+			if (!Directory.Exists(saveDir)) {
+				Directory.CreateDirectory(saveDir);
+			}
+			lock (_watchInfoList) {
+				watchInfo.SaveDir = saveDir;
+			}
+			
+			while (true) {
+				saveFilename = thread + ".html";
+				savePath = (saveFilename.Length != 0) ? Path.Combine(saveDir, saveFilename) : null;
+				for (numTries = 1; numTries <= maxTries; numTries++) {
+					lock (_watchInfoList) {
+						if (watchInfo.Stop) {
+							SetStatus(watchInfo, "Stopped by user");
+							return;
+						}
+						SetStatus(watchInfo, String.Format("Downloading page{0}", numTries == 1 ?
+							String.Empty : " (retry " + (numTries - 1).ToString() + ")"));
+					}
+					try {
+						page = GetToString(pageURL, pageAuth, savePath, ref pageCacheTime);
+						break;
+					}
+					catch (HTTP404Exception) {
+						lock (_watchInfoList) {
+							SetStatus(watchInfo, "Stopped, page not found");
+						}
+						return;
+					}
+					catch (HTTP304Exception) {
+						page = null;
+						break;
+					}
+					catch {
+						page = null;
+					}
+				}
+				pageGetTime = DateTime.Now;
+				if (page != null) {
+					List<string> links = GetLinks(page, pageURL);
+					List<string> imgs = new List<string>();
+					foreach (string link in links) {
+						if (link.IndexOf("/src/") != -1) {
+							if (!imgs.Contains(link)) {
+								imgs.Add(link);
+							}
+						}
+					}
+					for (int i = 0; i < imgs.Count; i++) {
+						saveFilename = URLFilename(imgs[i]);
+						savePath = (saveFilename.Length != 0) ? Path.Combine(saveDir, saveFilename) : null;
+						if ((savePath != null) && !File.Exists(savePath)) {
+							for (numTries = 1; numTries <= maxTries; numTries++) {
+								lock (_watchInfoList) {
+									if (watchInfo.Stop) {
+										SetStatus(watchInfo, "Stopped by user");
+										return;
+									}
+									SetStatus(watchInfo, String.Format("Downloading image {0} " + 
+										"of {1}{2}", i + 1, imgs.Count, numTries == 1 ? String.Empty :
+										" (retry " + (numTries - 1).ToString() + ")"));
+								}
+								try {
+									GetToFile(imgs[i], imgAuth, savePath);
+									break;
+								}
+								catch {
+								}
+							}
+						}
+					}
+					page = null;
+				}
+				while (true) {
+					waitRemain = waitSeconds - ((TimeSpan)(DateTime.Now - pageGetTime)).TotalSeconds;
+					if (waitRemain <= 0.0) {
+						break;
+					}
+					lock (_watchInfoList) {
+						if (watchInfo.Stop || watchInfo.OneTime) {
+							SetStatus(watchInfo, watchInfo.Stop ? "Stopped by user" :
+								"Stopped, download finished");
+							return;
+						}
+						Thread.Sleep(100);
+						SetStatus(watchInfo, String.Format("Waiting {0:0} seconds", waitRemain));
+					}
+					Thread.Sleep(500);
+				}
+			}
+		}
+	}
+
+	public class ByteBuff {
+		public byte[] Data;
+		public int Length;
+
+		public ByteBuff(byte[] data, int len) {
+			Data = data;
+			Length = len;
+		}
+	}
+
+	public class WatchInfo {
+		public int ListIndex;
+		public bool Stop;
+		public Thread WatchThread;
+		public string PageURL;
+		public string PageAuth;
+		public string ImageAuth;
+		public int WaitSeconds;
+		public bool OneTime;
+		public string SaveDir;
+	}
+
+	public class HTTP404Exception : Exception {
+		public HTTP404Exception() {
+		}
+	}
+
+	public class HTTP304Exception : Exception {
+		public HTTP304Exception() {
+		}
+	}
+}
